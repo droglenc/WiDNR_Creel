50fe21e963fb15b4094e1d91dbcb92f6e1bbe88c
Derek Ogle <droglenc@users.noreply.github.com>
2019-11-21 22:05:08 -0600

    Reorganized structure

---
+++ LSuperior/zzzOgleOnly/zzzOld/LS_OPEN_2014_1Month/Ashland_Open_2014_info.R
@@ -0,0 +1,16 @@
+## Select route from the following: "Ashland","Bayfield","Cornucopia/Port Wing",
+##    "Little Sand Bay","Red Cliff","Saxon","Superior", "Washburn"
+LOCATION <- "Ashland"
+
+## Enter start and end dates (must be MM/DD/YYYY)
+START_DATE <- "05/16/2014"
+END_DATE <- "09/30/2014"
+
+## Enter day length for each month
+DAY_LENGTH <- c(Jan=00,Feb=00,Mar=00,Apr=00,May=14,Jun=14,
+                Jul=14,Aug=14,Sep=13,Oct=00,Nov=00,Dec=00)
+
+## Enter names of counts, interviews, and fish data files
+CNTS_FILE <- "qry_ow_counts_4R.xlsx"
+INTS_FILE <- "qry_ow_interviews_4R.xlsx"
+FISH_FILE <- "qry_ow_fish_4R.xlsx"
